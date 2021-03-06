1c15c396799ec892afde87c2eb73884fb2f3fc7f
Fred Ma <fredma1234@gmail.com>
2021-03-06 15:24:32 -0500

    Added Apollo Client and HashRouter to client index.js Added ID to UserType Fixed RootQueryType

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { HashRouter } from 'react-router-dom';
+
+// import App from './components/App';
+
+export const client = new ApolloClient({
+    cache: new InMemoryCache({
+        typePolicies: {
+            user: {
+                keyFields: ['id']
+            }
+        }
+    }),
+    uri: 'http://localhost:4000/graphql',
+});
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <HashRouter>
+                <div>Auth Provider</div>
+                {/*<App />*/}
+            </HashRouter>
+        </ApolloProvider>
+    );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+render(<Root />, document.querySelector('#root'));
